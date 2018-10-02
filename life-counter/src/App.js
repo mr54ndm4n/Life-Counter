4a7768680ca0e9962fddf6ce1de1810e7eb89911
oat9002 <oat9002@hotmail.com>
2018-10-03 00:17:43 +0700

    Initialize with create-react-app

---
+++ life-counter/src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import LifeCounter from './LifeCounter';
+import './App.css';
+
+class App extends Component {
+  state = {
+    amount: 2,
+    lcList: [1, 2, 3, 4, 5, 6, 7, 8],
+    currentlist: [1, 2]
+  }
+ 
+  render() {
+    const LifeCounterList = ({ lcList }) => {
+      return (
+        <div className="box">
+          {
+            lcList.map((lc) => {
+              return <LifeCounter player={ lc } />
+            })
+          }
+        </div>
+      );
+
+    }
+
+    return (
+      <div id="page">
+        <div>
+            <center>
+              <h1 className="title is-1">Life Counter</h1>
+              <h3 className="title is-3">Player Amount</h3>
+                <p className="control playerSelect">
+                  <span className="select is-large">
+                    <select id="amount"
+                      onChange={
+                        () => {
+                          var newAmount = document.getElementById('amount').value;
+                          this.setState({
+                            amount: newAmount,
+                            currentlist: this.state.lcList.slice(0, newAmount)
+                          });
+                        }
+                      }>
+                      <option>2</option>
+                      <option>3</option>
+                      <option>4</option>
+                      <option>5</option>
+                      <option>6</option>
+                      <option>7</option>
+                      <option>8</option>
+                    </select>
+                  </span>
+                </p>
+                <LifeCounterList lcList={this.state.currentlist} />
+            </center>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ life-counter/src/LifeCounter.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import $ from "jquery";
+
+class LifeCounter extends Component{
+    state = {
+        score: 8000,
+        playerid: 'player ' + this.props.player
+    }
+
+    buttonClickHandler = () => {
+        var opt = $("#op" + this.state.playerid + " :selected").text();
+        var newScore = this.state.score;
+
+        /*Calculate value*/
+        if(opt === '-'){
+            newScore -= parseInt(document.getElementById(this.state.playerid).value)
+        }
+        else if(opt === '+'){
+            newScore += parseInt(document.getElementById(this.state.playerid).value)
+        }
+
+        /*Change value*/
+        if(isNaN(newScore)){
+            alert('Invalid value');
+        }
+        else{
+            alert(newScore);
+            this.setState({
+                score: newScore
+            })
+        }
+
+        /*Clear textbox*/
+        document.getElementById(this.state.playerid).value = "";
+    }
+
+    render(){
+        return (
+            <div className="container">
+                <div className="box">
+                    <h2 className="title is-2">Player {this.props.player}</h2>
+                    <h3 className="title is-3">Score: {this.state.score}</h3>
+
+                    {/* Change Value Form */}
+                    <p className="control has-addons has-addons-centered">
+                        <span className="select is-large">
+                            <select id={'op' + this.state.playerid}>
+                                <option>-</option>
+                                <option>+</option>
+                            </select>
+                        </span>
+                        <input className="input is-expanded is-large" type="text" id={ this.state.playerid }/>
+                        <button className="button is-large is-primary" onClick={ this.buttonClickHandler }>OK               </button>
+                    </p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default LifeCounter;
